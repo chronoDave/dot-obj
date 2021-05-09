2b17aed37aeadd960077ce15c7ff64df127ed508
chronoDave <davidwolters6758@gmail.com>
2021-05-09 17:33:33 +0200

    Fixed get type definition

--- src/get.ts
+++ src/get.ts
@@ -1,4 +1,4 @@
-export default (object: { [key: string]: unknown }, path: string) => {
+export default (object: { [key: string]: unknown }, path: string): unknown => {
   const keys = path.split('.');
 
   for (let i = 0; i < keys.length; i += 1) {
