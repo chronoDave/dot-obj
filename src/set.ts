9510c31b8d8a9f44cb5a053430ef81f6aeba178f
David Wolters <david.wolters@OJ.LOCAL>
2021-05-11 12:30:39 +0200

    Fixed prototype pollution

--- src/set.ts
+++ src/set.ts
@@ -5,6 +5,7 @@ export default (object: { [key: string]: unknown }, path: string, value: any) =>
   for (let i = 0; i < keys.length; i += 1) {
     const key = keys[i];
 
+    if (key === '__proto__' || key === 'constructor') continue
     if (typeof object[key] !== 'object') object[key] = {};
     if (i === keys.length - 1) object[key] = value;
     // @ts-ignore
